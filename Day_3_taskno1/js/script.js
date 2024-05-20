dfb4c99127864b9eaa49b045899ee4947711a4de
Hemapriya182024 <hemakarthikeyan2024@gmail.com>
2024-05-20 19:08:13 +0530

    first commit

---
+++ Day_3_taskno1/js/script.js
@@ -0,0 +1,10 @@
+let obj1={name:"Person1",
+          age:5
+}
+let obj2={age:5,
+    name:"Person1"
+}
+if(true)
+    {
+        console.log(obj1==obj2)
+    }
---
+++ Day_3_taskno2/js/script.js
@@ -0,0 +1,14 @@
+function reqListener() {
+    const countryArr=JSON.parse(this.responseText);
+
+    for(let i=0;i<=countryArr.length-1;i++)
+        {
+            console.log(countryArr[i].flags.png)
+        }
+  }
+  
+  const req = new XMLHttpRequest();
+  req.addEventListener("load", reqListener);
+  req.open("GET", "https://restcountries.com/v3.1/all");
+  req.send();
+  
